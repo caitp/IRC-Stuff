5a40d785cda5231640e48924c997fd179289b7d9
Paige Thompson <erratic@erratic-Latitude-D430.(none)>
2013-11-25 09:53:35 -0800

    something that works, finally

--- irssi_facebook_linkcatcher/catcher.pl
+++ irssi_facebook_linkcatcher/catcher.pl
@@ -1,4 +1,4 @@
-use Facebook::Graph;
+#use Facebook::Graph;
 use LWP::Simple;                # From CPAN
 use JSON qw( decode_json );     # From CPAN
 use Data::Dumper;               # Perl core module
@@ -97,36 +97,79 @@ sub sendto_facebook_stream
     my $config = get_config();
     if (!exists($config->{'token'}))
     {
-        my $fb = Facebook::Graph->new(
-# desktop => 0,
-                app_id => $config->{'app_id'},
-                secret => $config->{'app_secret'},
-                postback => $config->{'return_url'}
-                );
-        my $uri = $fb->authorize
-            ->extend_permissions(qw( email publish_stream ))
-            ->set_display('popup')
-            ->uri_as_string;
-        return URI::Encode->new( { encode_reserved => 0 } )->encode($uri);
+#TODO implement IO::Async for login but not right now since I already have a token. 
+#my $fb = Facebook::Graph->new(
+#          app_id => $config->{'app_id'},
+#          secret => $config->{'app_secret'},
+#          postback => $config->{'return_url'}
+#          );
+#  my $uri = $fb->authorize
+#      ->extend_permissions(qw( email publish_stream ))
+#      ->set_display('popup')
+#      ->uri_as_string;
+#  return URI::Encode->new( { encode_reserved => 0 } )->encode($uri);
+
+        @cmd ="curl -s \"https://graph.facebook.com/oauth/access_token?client_id=$config->{'app_id'}&client_secret=$config->{'app_secret'}&grant_type=client_credentials\"";
+        print @cmd;
+        print "now set your token in your config";
+#        return;
+#        my $response = timeout_command($timeout, @cmd);
+#        print "$response" if (defined $response);
 
     }
     else 
     {
-        exec("perl -e \"
-        use Facebook::Graph;
-        Facebook::Graph->new(
-                app_id => '" . $config->{'app_id'} . "',
-                secret => '" . $config->{'app_secret'} . "',
-                postback => '" . $config->{'return_url'} . "',
-                )->access_token(\"" . $config->{'token'} . "\")->fb->add_post->set_message('" . $url . "')->publish; \" & ");
+# exec("perl -e \"
+# use Facebook::Graph;
+# Facebook::Graph->new(
+#         app_id => '" . $config->{'app_id'} . "',
+#         secret => '" . $config->{'app_secret'} . "',  # fuck no, this ain't php 
+#         postback => '" . $config->{'return_url'} . "',
+#         )->access_token(\"" . $config->{'token'} . "\")->fb->add_post->set_message('" . $url . "')->publish; \" & ");
+
+#http://search.cpan.org/~sparky/Net-Curl-Simple-0.13/lib/Net/Curl/Simple/Async.pm so many choices, which one is the right one.
+        my $timeout = 10;
+        my $auth_token = $config->{'token'};
+        my $title = $where . ' ' . $channel;
+#https://developers.facebook.com/docs/reference/api/post/
+        @cmd ="curl -s -F \"access_token=$auth_token\" -F \"link=$url\" -F \"name=$title\" -F \"caption=Caption for the link\" \"https://graph.facebook.com/irclinks/feed\"";
+        print @cmd;
+#        return;
+        my $response = timeout_command($timeout, @cmd);
+        print "$response" if (defined $response);
+
     }
+}
+
+# a way to run commands asynchronously from perl, but will it work from irssi... 
+#http://stackoverflow.com/questions/1752357/how-can-i-run-a-system-command-in-perl-asynchronously
+sub timeout_command {
+    my $timeout = (shift);
+    my @command = @_;
+    undef $@;
+    my $return  = eval {
+        local($SIG{ALRM}) = sub {die "timeout";};
+        alarm($timeout);
+        my $response;
+        open(CMD, '-|', @command) || die "couldn't run @command: $!\n";
+        while(<CMD>) {
+            $response .= $_;
+        }
+        close(CMD) || die "Couldn't close execution of @command: $!\n";
+        $response;
+    };
+    alarm(0);
+    if ($@) {
+        warn "@cmd failure: $@\n";
+    }
+    return $return;
 }
 
 #handlers 
 Irssi::signal_add_last("message public", "url_public");
 Irssi::signal_add_last("message private", "url_private");
 #Irssi::command_bind("url", "url_cmd");
-print "loaded and ready.";
+#print "loaded and ready.";
 
 
 
